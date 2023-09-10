945e913c6f64569071650a136379875bc3a30a0d
AbidemiT <tiamiyusikiruabidemi@gmail.com>
2023-09-10 15:37:40 +0100

    custom cursor and style.scss to styles.module.scss

--- src/app/component/Carousel/TextCarousel.tsx
+++ src/app/component/Carousel/TextCarousel.tsx
@@ -1,34 +1,37 @@
-import './style.scss';
+import Styles from './styles.module.scss';
+import Ticker from "framer-motion-ticker";
 
 const TextCarousel = () => {
     return (
         <>
-            <div className="devfest-carousel__wrapper">
-                <div className="devfest-carousel text">
-                    <div className="text-carousel">
-                        <p>Devfest Ibadan 2023// Dec Sat 2nd</p>
-                        <span className="-icon">
-                            <i className="diagonal-arrow"></i>
-                        </span>
-                    </div>
-                    <div className="text-carousel">
-                       <p>Devfest Ibadan 2023// Dec Sat 2nd</p>
-                        <span className="-icon">
-                            <i className="diagonal-arrow"></i>
-                        </span>
-                    </div>
-                    <div className="text-carousel">
-                       <p>Devfest Ibadan 2023// Dec Sat 2nd</p>
-                        <span className="-icon">
-                            <i className="diagonal-arrow"></i>
-                        </span>
-                    </div>
-                    <div className="text-carousel">
-                       <p>Devfest Ibadan 2023// Dec Sat 2nd</p>
-                        <span className="-icon">
-                            <i className="diagonal-arrow"></i>
-                        </span>
-                    </div>
+            <div className={Styles["devfest__carousel__wrapper"]}>
+                <div className={Styles["devfest__carousel"]}>
+                    <Ticker duration={60}>
+                        <div className={Styles["text__carousel"]}>
+                            <p>Devfest Ibadan 2023// Dec Sat 2nd</p>
+                            <span className="__icon">
+                                <i className="diagonal__arrow"></i>
+                            </span>
+                        </div>
+                        <div className={Styles["text__carousel"]}>
+                            <p>Devfest Ibadan 2023// Dec Sat 2nd</p>
+                            <span className="__icon">
+                                <i className="diagonal__arrow"></i>
+                            </span>
+                        </div>
+                        <div className={Styles["text__carousel"]}>
+                            <p>Devfest Ibadan 2023// Dec Sat 2nd</p>
+                            <span className="__icon">
+                                <i className="diagonal__arrow"></i>
+                            </span>
+                        </div>
+                        <div className={Styles["text__carousel"]}>
+                            <p>Devfest Ibadan 2023// Dec Sat 2nd</p>
+                            <span className="__icon">
+                                <i className="diagonal__arrow"></i>
+                            </span>
+                        </div>
+                    </Ticker>
                 </div>
             </div>
         </>
